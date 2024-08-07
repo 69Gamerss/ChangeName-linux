8af0c500e9673d097edb68e0c3636ee916bbab20
69Gamerss <69Gamerss@gmail.com>
2024-08-07 14:16:29 +0300

    Update ChangeName.sh

--- ChangeName.sh
+++ ChangeName.sh
@@ -27,6 +27,6 @@ hostnamectl set-hostname $NEW_HOSTNAME
 # Display a message indicating the hostname has been changed
 echo "Hostname changed to $NEW_HOSTNAME. You may need to reboot for all changes to take effect."
 
-sudo exec bash 
+sudo reboot now 
 
 exit 0
